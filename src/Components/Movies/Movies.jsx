02641c6b4f9b4e9e425343879b283b37bc010235
yazyounas <yazyounas@gmail.com>
2023-05-18 14:43:51 +0100

    user is able to filter out movies with cast, director, genre and title

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+
 import "./Movie.css"
+import React, { useState }  from 'react';
+import movieData from "./MoviesData.json";
+
+
 
 const Movies = () => {
+  const [listQuery, setListQuery] = useState("")
+
+const filteredMovies = movieData.filter((movie) => {
+  const { title, cast, genre, director } = movie;
+  const query = listQuery.toLowerCase();
+  console.log(query)
+  
+  return (
+    title.toLowerCase().includes(query) ||
+    cast.some((actor) => actor.toLowerCase().includes(query)) ||
+    director.toLowerCase().includes(query) ||
+    genre.toLowerCase().includes(query)
+   
+  )
+})
+
+ 
+  
   return (
     <div>
-      <h1>Filter Movies List</h1>
+
+<form>
+    <label htmlFor="listQuery">Query Movies:</label>
+    <input 
+    id="listQuery"
+    type="text"
+    value={listQuery}
+    onChange={(event) => setListQuery(event.target.value)}
+    placeholder="Search by Title, Director, Genre, or Actor/Actress"
+    />
+
+   </form>
+   <table>
+    <thead>
+      <tr>
+   <th>Title</th>
+   <th>Director</th>
+   <th>year</th>
+   <th>Genre</th>
+   <th>Cast</th>
+   <th>Average Rating</th>
+   </tr>
+   </thead>
+
+   {filteredMovies.map((movie) => (
+    
+    <tr key={movie.id}>
+      <td>{movie.title}</td>
+      <td>{movie.director}</td>
+      <td>{movie.year}</td>  
+      <td>{movie.genre}</td>
+      <td>{movie.cast.join(", ")}</td>
+      <td>{movie.averageRating}</td>
+      
+    </tr>
+   ))}
+   </table>
     </div>
   )
 }
